02b9514cef74240dc53fa876a9f931c0b69a1fa0
Mir Nawaz <you@example.com>
2016-02-18 12:02:59 +0500

    access logging to file

--- server.js
+++ server.js
@@ -4,14 +4,38 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const chalk = require('chalk');
+const FileStreamRotator = require('file-stream-rotator')
+const fs = require('fs');
+const morgan = require('morgan');
+
+// Express
+const app = express();
+
 // MongoDB
 mongoose.connect('mongodb://localhost/demo');
 const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log(chalk.green("we're connected!"));
 });
-// Express
-const app = express();
+
+// create a write stream (in append mode)
+var logDirectory = __dirname + '/log';
+
+// ensure log directory exists
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+
+// create a rotating write stream
+var accessLogStream = FileStreamRotator.getStream({
+  filename: logDirectory + '/access-%DATE%.log',
+  frequency: 'daily',
+  verbose: false,
+  date_format: "YYYY-MM-DD"
+});
+
+// setup the logger
+app.use(morgan('combined', {stream: accessLogStream}));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
